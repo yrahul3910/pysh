9453cc5a8f5553a17e0cb2d269ce6e69bf322150
Rahul Yedida <rahul.yedida@pm.me>
2023-09-18 20:14:57 -0400

    Added more tests

--- pysh/run_tests.sh
+++ pysh/run_tests.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
 # Install pysh
-echo "(1/3) Installing pysh..."
+echo "(1/4) Installing pysh..."
 cmake -S . -B build
 cd build && make && sudo make install
 
 # Transpile pysh to Python
-echo "(2/3) Transpiling pysh to Python..."
+echo "(2/4) Transpiling pysh to Python..."
 cd ../tests || exit
 pysh tests.pysh
 mv out.py test_all.py
 
 # Run tests
-echo "(3/3) Running tests..."
+echo "(3/4) Running tests..."
 python3 -m pip install pytest
 pytest
+
+# Run CTest
+echo "(4/4) Running CTest..."
+cd ../build
+ctest -R test --output-on-failure
--- pysh/tests/test_transpile.cpp
+++ pysh/tests/test_transpile.cpp
@@ -10,3 +10,43 @@ TEST_CASE("pure Python line is not modified", "[transpile]")
     process_line(line, ss);
     REQUIRE(ss.str() == "print('Hello, world!')\n");
 }
+
+TEST_CASE("indents are preserved", "[transpile]")
+{
+    std::string line = "    print('Hello, world!')";
+    std::stringstream ss;
+    process_line(line, ss);
+    REQUIRE(ss.str() == "    print('Hello, world!')\n");
+}
+
+TEST_CASE("tabs are preserved", "[transpile]")
+{
+    std::string line = "\tprint('Hello, world!')";
+    std::stringstream ss;
+    process_line(line, ss);
+    REQUIRE(ss.str() == "    print('Hello, world!')\n");
+}
+
+TEST_CASE("comments are removed", "[transpile]")
+{
+    std::string line = "print('Hello, world!') # This is a comment";
+    std::stringstream ss;
+    process_line(line, ss);
+    REQUIRE(ss.str() == "print('Hello, world!') \n");
+}
+
+TEST_CASE("template literals are parsed", "[transpile]")
+{
+    std::string line = "int`cat file.txt`";
+    std::stringstream ss;
+    process_line(line, ss);
+    REQUIRE(ss.str() == "_ = subprocess.Popen(f'cat file.txt', shell=True, cwd=os.getcwd(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].decode('utf-8').rstrip()\ntry:\n    _ = int(_)\nexcept ValueError:\n    raise\n_\n");
+}
+
+TEST_CASE("list template parses to comprehension", "[transpile]")
+{
+    std::string line = "list.int`cat file.txt`";
+    std::stringstream ss;
+    process_line(line, ss);
+    REQUIRE(ss.str() == "_ = subprocess.Popen(f'cat file.txt', shell=True, cwd=os.getcwd(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].decode('utf-8').rstrip()\n_ = [int(x) for x in _]\n");
+}
