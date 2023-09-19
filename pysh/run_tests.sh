443c2e1fad7fd4917c3dcb525afb0eed8ff32b3b
Rahul Yedida <rahul.yedida@pm.me>
2023-09-18 21:28:44 -0400

    Fix coverage reporting

--- pysh/run_tests.sh
+++ pysh/run_tests.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
 
 # Install pysh
-echo "(1/4) Installing pysh..."
+echo "(1/5) Installing pysh..."
 cmake -S . -B build
 cd build && make && sudo make install
 
 # Transpile pysh to Python
-echo "(2/4) Transpiling pysh to Python..."
+echo "(2/5) Transpiling pysh to Python..."
 cd ../tests || exit
 pysh tests.pysh
 mv out.py test_all.py
 
 # Run tests
-echo "(3/4) Running tests..."
+echo "(3/5) Running tests..."
 python3 -m pip install pytest
 pytest
 
 # Run CTest
-echo "(4/4) Running CTest..."
+echo "(4/5) Running CTest..."
 cd ../build
-ctest -R test --output-on-failure
+make test
+
+echo "(5/5) Uploading coverage report to Codecov..."
+curl --retry 5 -s https://codecov.io/bash > codecov.bash && bash codecov.bash -Z -X gcov
