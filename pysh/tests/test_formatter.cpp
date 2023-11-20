ef5330800d05673fd83da91dd0c78f606d558e3b
Rahul Yedida <rahul.yedida@pm.me>
2023-11-20 17:52:15 -0500

    Added test for custom formatter

---
+++ pysh/tests/test_formatter.cpp
@@ -0,0 +1,9 @@
+#include <catch2/catch_test_macros.hpp>
+#include <sstream>
+#include "formatter.hpp"
+
+TEST_CASE("custom formatter works correctly", "[formatter]")
+{
+    type_formatter formatter("custom", 0);
+    REQUIRE(formatter.format() == "try:\n    _ = custom(_)\nexcept NameError:\n    raise NameError('custom is not a valid custom formatter.')\nexcept TypeError:\n    raise TypeError('custom is not callable, or takes too many args.')\n");
+}
