733644e927cc5309115683156860eab3bb44aedd
Rahul Yedida <rahul.yedida@pm.me>
2024-04-30 22:28:36 -0400

    Bug fix in safe mode

--- pysh/src/main.cpp
+++ pysh/src/main.cpp
@@ -14,8 +14,14 @@ enum ErrorCode {
     ERR_FILE_EXISTS_SAFE_MODE = 3
 };
 
-void transpile_file(const std::string& filename, const std::string& out_filename)
+void transpile_file(const std::string& filename, const std::string& out_filename, bool safe_mode=false)
 {
+    if (safe_mode && fs::exists(fs::path(out_filename))) {
+        std::cerr << out_filename << " already exists." << std::endl;
+        std::cerr << "Note: Safe mode is enabled. If you wish to overwrite the file, please disable safe mode." << std::endl;
+        exit(ERR_FILE_EXISTS_SAFE_MODE);
+    }
+
     std::ifstream fin(filename.c_str());
     std::ofstream fout(out_filename.c_str());
 
@@ -63,7 +69,7 @@ int main(int argc, char* argv[])
     }
 
     fs::path filename = vm["input"].as<std::string>();
-    if (!fs::exists(filename)) {
+    if (!fs::exists(filename) && !vm.count("recursive")) {
         std::cerr << argv[1] << " does not exist." << std::endl;
         return ERR_FILE_NOT_FOUND;
     }
@@ -80,17 +86,18 @@ int main(int argc, char* argv[])
         std::cerr << "Note: Safe mode is enabled. If you wish to overwrite the file, please disable safe mode." << std::endl;
         return ERR_FILE_EXISTS_SAFE_MODE;
     }
-    transpile_file(filename, out_filename);
 
     // If recursive option is set, transpile all files in the directory
     if (vm.count("recursive")) {
         for (const auto& entry : fs::recursive_directory_iterator(filename.parent_path())) {
             if (entry.path().extension() == ".pysh") {
                 // transpile_file will auto-change the extension
-                transpile_file(entry.path().string(), "");
+                transpile_file(entry.path().string(), "", safe_mode);
             }
         }
-    }
+    } else {
+        transpile_file(filename, out_filename);
+  }
 
     if (!vm.count("transpile")) {
         // Run the code
