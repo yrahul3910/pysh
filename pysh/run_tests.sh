863dbce7fb7c5dce43dafd0119579fc268518db1
Rahul Yedida <yrahul3910@gmail.com>
2024-12-31 16:55:27 -0500

    Update Codecov action to v5

--- pysh/run_tests.sh
+++ pysh/run_tests.sh
@@ -1,25 +1,22 @@
 #!/usr/bin/env bash
 
 # Install pysh
-echo "(1/5) Installing pysh..."
+echo "(1/4) Installing pysh..."
 cmake -S . -B build
 cd build && make && sudo make install
 
 # Transpile pysh to Python
-echo "(2/5) Transpiling pysh to Python..."
+echo "(2/4) Transpiling pysh to Python..."
 cd ../tests || exit
 pysh tests.pysh
-mv out.py test_all.py
+mv tests.py test_all.py
 
 # Run tests
-echo "(3/5) Running tests..."
-python3 -m pip install pytest
-pytest
+echo "(3/4) Running tests..."
+python3 -m pip install pytest pytest-cov
+pytest --cov=./ --cov-report=xml
 
 # Run CTest
-echo "(4/5) Running CTest..."
+echo "(4/4) Running CTest..."
 cd ../build
 make test
-
-echo "(5/5) Uploading coverage report to Codecov..."
-curl --retry 5 -s https://codecov.io/bash > codecov.bash && bash codecov.bash -Z
